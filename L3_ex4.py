b89425e546e78cb5efe953e0cf1083913bcb9ea1
Xavier Celades <xcelades@gmail.com>
2020-12-03 21:30:44 +0100

    Final code for the fourth exercise

---
+++ L3_ex4.py
@@ -0,0 +1,13 @@
+import os
+
+
+def ex4():
+
+    print("To create a container, run de two ecerises: ")
+
+    os.system("python L3_ex2.py")
+    os.system("python L3_ex3.py")
+
+
+if __name__ == "__main__":
+    ex4()
