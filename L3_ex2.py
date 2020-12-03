13440837b6b9896989ddbe51d020617248eab002
Xavier Celades <xcelades@gmail.com>
2020-12-03 21:33:38 +0100

    Final code for the second  exercise

---
+++ L3_ex2.py
@@ -0,0 +1,35 @@
+import os
+
+
+def ex2():
+
+    # Ask the user
+    print("Choose a number: ")
+    total_files = input()
+    files = []
+
+    # Create loop variable
+    n_total_files = int(total_files)
+
+    # get the name
+    print("Insert the name of the files:")
+    for k in range(n_total_files):
+
+        print("Name of the file " + str(k+1) + ":")
+
+        name_file = input()
+
+        files.append(name_file)
+
+    # Store
+    for k in range(n_total_files):
+        name = "ffmpeg -i {} ".format(files[k])
+
+    # Add name
+    name = str(name + "container.mp4")
+    os.system(name)
+
+
+if __name__ == "__main__":
+
+    ex2()
